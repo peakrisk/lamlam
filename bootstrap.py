d1e257011f4816a592d5491befee9b801f8ae8f4
John Gill <swfiua@gmail.com>
2015-11-02 17:09:43 -0400

    boot rest cog hello world

--- bootstrap.py
+++ bootstrap.py
@@ -2,14 +2,36 @@
 
 See: rst2py ?
 """
+import os
 from docutils.parsers import rst
 from docutils.nodes import document
 from docutils.utils import new_document
 from docutils.frontend import OptionParser
 
-import inspect
+COG = """
+from bootstrap import Bootstrap
 
-from cogapp import Cog
+# parse rest document
+
+boot = Bootstrap()
+
+rest = open("%(infile)s").read()
+
+boot.interpret(rest)
+
+#cog.out(boot.say())
+cog.out("hello world")
+"""
+
+COG_RUN = """
+if __name__ == '__main__':
+
+
+    from cogapp import Cog
+
+    Cog().main(['cog', '-cr', __file__])
+"""
+
 
 class Bootstrap:
 
@@ -22,28 +44,56 @@ class Bootstrap:
     def interpret(self, data):
 
         self.parser = rst.Parser()
-        self.document = new_document('.', settings)
+        self.document = new_document('.', self.settings)
 
-        self.parser.parse(data, document)
+        self.parser.parse(data, self.document)
 
     def say(self, *args, **kwargs):
-        """ Write a python cog file interpreting rst """
-        for item in data:
-            print(' ' * depth, item, type(item).name)
+        """ Write a python cog file to interpret rst """
+        self.dump(self.document)
 
-            for sub in item:
-                if str(sub) != sub:
-                    dump(sub, depth+1)
-        
-
-    def dump(self):
+    def dump(self, data, depth=0):
 
         for item in data:
             print(' ' * depth, item, type(item))
 
-            for sub in item:
-                if str(sub) != sub:
-                    dump(sub, depth+1)
+            name = item.__class__.__name__
+
+            method = getattr(self, name, self.unknown)
+
+            method()
+
+    def unknown(self, item):
+
+        print('unknown')
+
+    def section(self, item):
+
+        pass
+        
 
 
+if __name__ == '__main__':
 
+    import sys
+
+
+    for infile in sys.argv[1:]:
+
+        base = infile.lstrip('rest').rstrip("rst")
+        folder, module = os.path.split(base)
+
+        out_folder = 'code' + folder
+        os.makedirs(out_folder, exist_ok=True)
+        
+        outname = 'code' + base + 'py'
+        print(outname)
+        with open(outname, 'w') as outfile:
+            print('""" Bootstrap cogging """', file=outfile)
+            print(file=outfile)
+            print('"""[[[cog', file=outfile)
+            print(COG % locals(), file=outfile)
+            print(']]]"""', file=outfile)
+            print('#[[[end]]]', file=outfile)
+
+            print(COG_RUN, file=outfile)
