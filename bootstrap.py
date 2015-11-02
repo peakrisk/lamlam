b444604fa37ccc79369f8f333c94431f1f8f80ae
John Gill <swfiua@gmail.com>
2015-11-02 14:49:29 -0400

    cog bootstrap rest notes

--- bootstrap.py
+++ bootstrap.py
@@ -1,8 +1,11 @@
-""" Just write this thing
+""" Get a puppet up and running
 
-See: rst2py
+See: rst2py ?
 """
-import docutils
+from docutils.parsers import rst
+from docutils.nodes import document
+from docutils.utils import new_document
+from docutils.frontend import OptionParser
 
 import inspect
 
@@ -10,13 +13,37 @@ from cogapp import Cog
 
 class Bootstrap:
 
-    def interpret(self, *args, **kwargs):
+    def __init__(self):
 
-        infile = kwarg.get('infile')
+        self.settings = OptionParser(
+            components=(rst.Parser,)
+            ).get_default_values()
 
-        for item in infile:
-            self.say(self.interpret(item))
-    
+    def interpret(self, data):
+
+        self.parser = rst.Parser()
+        self.document = new_document('.', settings)
+
+        self.parser.parse(data, document)
+
     def say(self, *args, **kwargs):
-        pass
-    
+        """ Write a python cog file interpreting rst """
+        for item in data:
+            print(' ' * depth, item, type(item).name)
+
+            for sub in item:
+                if str(sub) != sub:
+                    dump(sub, depth+1)
+        
+
+    def dump(self):
+
+        for item in data:
+            print(' ' * depth, item, type(item))
+
+            for sub in item:
+                if str(sub) != sub:
+                    dump(sub, depth+1)
+
+
+
